94d443200d8004ec2e3b625d692b66855848277a
Alazhry <aam881011@gmail.com]>
2022-09-16 19:08:42 +0200

    add photo to sponsor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import speakersData from './design-system/components/CreativeSpeakers/SpeakersDa
 import "./design-system/main.css"
 import OurPrograms from './design-system/components/OurPrograms/OurPrograms';
 import ourProgramsData from "./design-system/components/OurPrograms/OurProgramsData";
+import OurSponsors from "./design-system/components/OurSponsors/OurSponsors";
+import ourSponsors from './design-system/components/OurSponsors/OurSponsorsData';
 function App() {
   return (
     <div className="App">
@@ -13,13 +15,22 @@ function App() {
           )
         })}
       </div>
-      {ourProgramsData.map(item => {
-        return(
-          <OurPrograms img={item.img} title={item.title} 
-          time={item.time} alt={item.alt} description={item.description} 
-          name={item.name} roomNumber={item.roomNumber} key={item.id} />
-        )
-      })}
+      <div>
+        {ourProgramsData.map(item => {
+          return(
+            <OurPrograms img={item.img} title={item.title} 
+            time={item.time} alt={item.alt} description={item.description} 
+            name={item.name} roomNumber={item.roomNumber} key={item.id} />
+            )
+          })}
+      </div>
+      <div style={{display: 'flex', gap: "1rem"}}>
+          {ourSponsors.map(item => {
+            return(
+              <OurSponsors src={item.src} alt={item.alt} key={item.id} />
+            )
+          })}
+      </div>
     </div>
   );
 }
---
+++ src/design-system/components/OurSponsors/OurSponsorsData.jsx
@@ -0,0 +1,30 @@
+import sponsor1 from '../../../shared/assets/OurSponsors/sponsor1.jpg';
+import sponsor2 from '../../../shared/assets/OurSponsors/sponsor-img2.jpg';
+import sponsor3 from '../../../shared/assets/OurSponsors/sponsor-img3.jpg';
+import sponsor4 from '../../../shared/assets/OurSponsors/sponsor-img4.jpg';
+
+const OurSponsors = [
+    {
+        id:1,
+        src:sponsor1,
+        alt:"sponsor1",
+    },
+    {
+        id:2,
+        src:sponsor2,
+        alt:"sponsor2",
+    },
+    {
+        id:3,
+        src:sponsor3,
+        alt:"sponsor3",
+    },
+    {
+        id:4,
+        src:sponsor4,
+        alt:"sponsor4",
+    },
+];
+
+
+export default OurSponsors;
